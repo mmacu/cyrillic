784d0d797a3ab50c10ac46830f2b69134ab8e21d
Michael Macutkiewicz <m.macu@outlook.com>
2022-03-09 18:18:48 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import cyrillic from './cyrillic.json'
+import React, { useState, useEffect } from 'react'
+import { Container, CssBaseline, Button, Grid, Paper, Stack, Box, Typography } from '@mui/material'
 
 function App() {
+  const initializeProgress = () => (
+    cyrillic.map(element => ({data: element, metadata: {delayedUntil: Date.now(), correctAnswers: 0, incorrectAnswers: 0, }}))
+  )
+  const [progress, setProgress] = useState(initializeProgress())
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Container>
+        <Box sx={{ marginTop: 6, maxWidth: 440, marginLeft: 'auto', marginRight: 'auto' }}>
+          <Stack direction='column' spacing={2}>
+            <Box>
+              <Typography variant='h3' align='center'>Item</Typography>
+            </Box>
+            <Grid container spacing={2}>
+              <Grid item xs={4}>
+                <Button variant='outlined' fullWidth>A</Button>
+              </Grid>
+              <Grid item xs={4}>
+                <Button variant='outlined' fullWidth>B</Button>
+              </Grid>
+              <Grid item xs={4}>
+                <Button variant='outlined' fullWidth>C</Button>
+              </Grid>
+              <Grid item xs={4}>
+                <Button variant='outlined' fullWidth>D</Button>
+              </Grid>
+              <Grid item xs={4}>
+                <Button variant='outlined' fullWidth>E</Button>
+              </Grid>
+              <Grid item xs={4}>
+                <Button onClick={() => console.log(progress)} variant='outlined' fullWidth>F</Button>
+              </Grid>
+            </Grid>
+          </Stack>
+        </Box>
+      </Container>
+    </>
   );
 }
 
