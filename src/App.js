6b975ec8cc6d698c1a414a2fce81ac1476378afd
Michael Macutkiewicz <m.macu@outlook.com>
2022-03-09 22:13:49 +0100

    almost works

--- src/App.js
+++ src/App.js
@@ -3,27 +3,72 @@ import React, { useState, useEffect } from 'react'
 import { Container, CssBaseline, Button, Grid, Paper, Stack, Box, Typography } from '@mui/material'
 
 function App() {
-  // const initializeProgress = () =>
-  //   cyrillic.map((element) => ({
-  //     data: element,
-  //     metadata: { delayedUntil: Date.now(), correctAnswers: 0, incorrectAnswers: 0 },
-  //   }))
-
-  const [alreadyKnown, setAlreadyKnown] = useState([])
-
   const getRandomSubarray = (arr, size) => {
-    var shuffled = arr.slice(0), i = arr.length, temp, index;
+    var shuffled = arr.slice(0),
+      i = arr.length,
+      temp,
+      index
     while (i--) {
-      index = Math.floor((i + 1) * Math.random());
-      temp = shuffled[index];
-      shuffled[index] = shuffled[i];
-      shuffled[i] = temp;
+      index = Math.floor((i + 1) * Math.random())
+      temp = shuffled[index]
+      shuffled[index] = shuffled[i]
+      shuffled[i] = temp
     }
-    return shuffled.slice(0, size);
+    return shuffled.slice(0, size)
+  }
+
+  const getRandomInt = (max) => {
+    return Math.floor(Math.random() * max)
   }
 
   const getAnswers = (correct) => {
-    return getRandomSubarray(cyrillic.filter(element => element !== correct), 5)
+    const answers = getRandomSubarray(
+      cyrillic.filter((element) => element !== correct),
+      6
+    )
+    const randomIdx = getRandomInt(answers.length - 1)
+    answers[randomIdx] = currentLetter
+    return answers
+  }
+
+  const [allLetters, setAllLetters] = useState(cyrillic)
+  const [lettersLeft, setLettersLeft] = useState(cyrillic)
+  const [currentLetter, setCurrentLetter] = useState(cyrillic[0])
+  const [currentAnswers, setCurrentAnswers] = useState(getAnswers(cyrillic[0]))
+  const [showNextButton, setShowNextButton] = useState(false)
+  const [userAnswer, setUserAnswer] = useState(null)
+
+  useEffect(() => {
+    lettersLeft.shift()
+  }, [])
+
+  useEffect(() => {
+    setCurrentAnswers(getAnswers(currentLetter))
+  }, [currentLetter])
+
+  const submitAnswer = (answer) => {
+    if (showNextButton) {
+      return
+    }
+    setShowNextButton(true)
+    if (answer !== currentLetter) {
+      lettersLeft.push(currentLetter)
+    } 
+  }
+
+  const goNext = () => {
+    setCurrentLetter(lettersLeft.shift())
+    setShowNextButton(false)
+  }
+
+  const getButtonColor = (answer) => {
+    if (!showNextButton) {
+      return 'primary'
+    } else if (currentLetter === answer) {
+      return 'success'
+    } else {
+      return 'error'
+    }
   }
 
   return (
@@ -34,40 +79,20 @@ function App() {
           <Stack direction="column" spacing={2}>
             <Box>
               <Typography variant="h3" align="center">
-                Item
+                {currentLetter.uppercase}
               </Typography>
             </Box>
             <Grid container spacing={2}>
-              <Grid item xs={4}>
-                <Button variant="outlined" fullWidth>
-                  A
-                </Button>
-              </Grid>
-              <Grid item xs={4}>
-                <Button variant="outlined" fullWidth>
-                  B
-                </Button>
-              </Grid>
-              <Grid item xs={4}>
-                <Button variant="outlined" fullWidth>
-                  C
-                </Button>
-              </Grid>
-              <Grid item xs={4}>
-                <Button variant="outlined" fullWidth>
-                  D
-                </Button>
-              </Grid>
-              <Grid item xs={4}>
-                <Button onClick={() => console.log(getAnswers(cyrillic[0]))} variant="outlined" fullWidth>
-                  E
-                </Button>
-              </Grid>
-              <Grid item xs={4}>
-                <Button onClick={() => console.log('todo')} variant="outlined" fullWidth>
-                  F
-                </Button>
-              </Grid>
+              {currentAnswers.map((answer, idx) => (
+                <Grid item key={answer.uppercase} xs={4}>
+                  <Button variant="outlined" fullWidth style={{textTransform: 'none'}} color={getButtonColor(answer)} onClick={() => submitAnswer(answer, idx)}>
+                    {answer.name}
+                  </Button>
+                </Grid>
+              ))}
+                <Grid item xs={12}>
+                  <Button disabled={!showNextButton} size='large' fullWidth variant='contained' onClick={() => goNext()}>Next</Button>
+                </Grid>
             </Grid>
           </Stack>
         </Box>
